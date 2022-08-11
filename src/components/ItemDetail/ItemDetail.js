2cf24a5f928c9070b2aac801ca38a80b1a3e748a
Felipe <felipeparada99@hotmail.com>
2022-08-11 13:26:15 -0300

    arreglo estilos de itemdetail e item detail container

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -4,16 +4,21 @@ import '../Contador/Contador'
 import Contador from '../Contador/Contador'
 const ItemDetail = ({nombre,imgSrc,precio,stock,desc}) => {
     const onAdd = ()=>{
-        
+        console.log("click en el contador")
+    }
+
+    const handeOnClickDescripcion= (e)=>{
+        // e.stopPropagation()
+        console.log("click en container descripcion")
     }
 
     return (
-        <div className="containerDetalle">
+        <div className="containerDetalle" onClick={ ()=>{console.log("click en container deatlle")}}>
            <div className="contImg">
                 <img src={imgSrc} alt={nombre}/>
            </div>
 
-           <div className="contDesc">
+           <div className="contDesc" onClick={ handeOnClickDescripcion}>
                <h1>{nombre}</h1>
                <h3>${precio}</h3>
                <p>{desc}</p>
